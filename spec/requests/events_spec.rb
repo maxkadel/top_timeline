2d3e5a7cb2bb32093b0429ac919689821a4cb174
Max Kadel <max@curationexperts.com>
2021-02-07 10:43:40 -0500

    Add cancan for authorization stuff

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -16,11 +16,22 @@ RSpec.describe "/events", type: :request do
   # Event. As you add validations to Event, be sure to
   # adjust the attributes here as well.
   let(:user) { User.create!(email: "test@test.com", password: "password") }
+  let(:other_user) { User.create!(email: "someone_else@test.com", password: "password") }
+  let(:target_user_event) { Event.create! valid_attributes }
+  let(:other_user_event) { Event.create! someone_elses_event }
   let(:valid_attributes) {
     {
       start_time: Time.current,
       name: "Any string will do",
       user: user
+    }
+  }
+
+  let(:someone_elses_event) {
+    {
+      start_time: Time.current,
+      name: "This does not belong to you",
+      user: other_user
     }
   }
 
@@ -33,21 +44,38 @@ RSpec.describe "/events", type: :request do
 
   before do
     login_as(user)
+    other_user
   end
 
   describe "GET /index" do
     it "renders a successful response" do
-      Event.create! valid_attributes
+      target_user_event
       get events_url
+      expect(response).to be_successful
+    end
+
+    it "does not show someone else's events" do
+      target_user_event
+      other_user_event
+      get events_url
+      expect(response.body).to include("Any string will do")
+      expect(response.body).not_to include("This does not belong to you")
       expect(response).to be_successful
     end
   end
 
   describe "GET /show" do
     it "renders a successful response" do
-      event = Event.create! valid_attributes
+      event = target_user_event
       get event_url(event)
       expect(response).to be_successful
+    end
+
+    it "does not show someone else's event" do
+      event = other_user_event
+      expect do
+        get event_url(event)
+      end.to raise_error
     end
   end
 
@@ -90,7 +118,7 @@ RSpec.describe "/events", type: :request do
       it "renders a successful response (i.e. to display the 'new' template)" do
         post events_url, params: { event: invalid_attributes }
         # 422 is what's returned when the model fails validations
-        expect(response).to have_http_status(422)
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
@@ -122,7 +150,7 @@ RSpec.describe "/events", type: :request do
         event = Event.create! valid_attributes
         patch event_url(event), params: { event: invalid_attributes }
         # 422 is what's returned when the model fails validations
-        expect(response).to have_http_status(422)
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
