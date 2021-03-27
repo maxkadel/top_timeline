de56803756c2cc248545a1b3cd577cc8e3510c52
Max Kadel <mkadel@gmail.com>
2021-03-27 14:55:00 -0400

    Create surgery event from user login (#18)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :events, dependent: :destroy
+  after_save :create_or_update_surgery_event
+
+  # TODO: Only run this if it's new or the surgery date changes
+  def create_or_update_surgery_event
+    surgery_event = Event.find_or_create_by(name: 'Surgery', user_id: id)
+    surgery_event.start_time = surgery_date.to_datetime if surgery_date
+    surgery_event.save!
+  end
 end
